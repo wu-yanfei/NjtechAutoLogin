08ab6333cb0f510fc63e4b3f2d1afdd9f58da485
wu-yanfei <46380108+wu-yanfei@users.noreply.github.com>
2024-02-27 19:39:59 +0800

    Update login_openwrt.sh
    适应新版登录

--- login_openwrt.sh
+++ login_openwrt.sh
@@ -9,6 +9,9 @@ CHANNEL=cmcc
 
 WANIP=$(ifconfig eth0 | grep 'inet addr' | cut -d ':' -f2 | cut -d ' ' -f1)
 
+LOGIN_URL="http://10.50.255.11:801/eportal/portal/login"
+LOGIN_REQUEST_URL="${LOGIN_URL}?callback=dr1003&login_method=1&user_account=%2C1%2C${USERID}%40${CHANNEL}&user_password=${PASSWORD}&wlan_user_ip=${WANIP}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.1.3&terminal_type=2&lang=zh-cn&v=8166&lang=zh"
+
 url="http://www.baidu.com"
 status=$(curl -IsS -m 5 $url | head -n 1 | cut -d ' ' -f 2)
 
@@ -16,16 +19,6 @@ if [ "$status" = "200" ]; then
     echo "Internet connection is active!" | logger
 else
     echo "Internet connecting..." | logger
-    curl "http://10.50.255.11:801/eportal/?c=ACSetting&a=Login&protocol=http:&hostname=10.50.255.11&iTermType=1&wlanuserip=$WANIP&wlanacip=null&wlanacname=me60-2&mac=00-00-00-00-00-00&ip=$WANIP&enAdvert=0&queryACIP=0&jsVersion=2.4.3&loginMethod=1" \
-      -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-      -H 'Accept-Language: zh-CN,zh;q=0.9' \
-      -H 'Cache-Control: max-age=0' \
-      -H 'Connection: keep-alive' \
-      -H 'Content-Type: application/x-www-form-urlencoded' \
-      -H 'Origin: http://10.50.255.11' \
-      -H 'Referer: http://10.50.255.11/' \
-      -H 'Upgrade-Insecure-Requests: 1' \
-      -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' \
-      --data-raw "DDDDD=%2C0%2C$USERID%40$CHANNEL&upass=$PASSWORD&R1=0&R2=0&R3=0&R6=0&para=00&0MKKey=123456&buttonClicked=&redirect_url=&err_flag=&username=&password=&user=&cmd=&Login=&v6ip="
+    curl -s "$LOGIN_REQUEST_URL"
     echo "Internet connection is active!" | logger
 fi
